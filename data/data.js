47f505190166e2945a22fde306b1e131fdc87752
pee47kay <papakowdadson@gmail.com>
2024-03-03 23:59:02 +0000

    almost done with Potfolio

---
+++ data/data.js
@@ -0,0 +1,53 @@
+const projectsData = [
+  {
+    title: "SalvageMe",
+    image: "./assets/imageicon/splashIcon.png",
+    type: "Web",
+    description: "A Digital Transport System",
+    appLink: { name: "SalvageMe", url: "https://salvage-me.vercel.app/" },
+    tools: ["React", "Nodejs"],
+    otherLink: [
+      { name: "Ayoba", url: "www.google.com" },
+      { name: "Admin", url: "https://salvage-me-admin.vercel.app/" },
+    ],
+  },
+  {
+    title: "Trofare",
+    image: "./assets/imageicon/splashIcon.png",
+    type: "Mobile",
+    description: "A Digital Transport System",
+    appLink: {
+      name: "Trofare",
+      url: "https://play.google.com/store/apps/details?id=com.aimscommunity.trofare",
+    },
+    tools: ["Flutter", "Nodejs", "MongoDb", "Firebase"],
+    otherLink: [
+      {
+        name: "Trofare",
+        url: "https://play.google.com/store/apps/details?id=com.aimscommunity.trofare",
+      },
+      {
+        name: "Trofare Admin",
+        url: "https://play.google.com/store/apps/details?id=com.aimscommunity.trofareAdmin",
+      },
+      {
+        name: "Trofare Driver",
+        url: "https://play.google.com/store/apps/details?id=com.aimscommunity.trofare",
+      },
+      {
+        name: "Trofare Intercity",
+        url: "https://play.google.com/store/apps/details?id=com.aimscommunity.trofare",
+      },
+    ],
+  },
+  {
+    title: "C-Auth",
+    image: "./assets/imageicon/splashIcon.png",
+    type: "Web",
+    description: "Awarding government projects with Web 3",
+    appLink: { name: "C-Auth", url: "https://c-auth.vercel.app/" },
+    tools: ["Flutter", "Nodejs", "MongoDb", "Firebase"],
+    otherLink: [{ name: "C-Auth", url: "https://c-auth.vercel.app/" }],
+  },
+];
+
---
+++ script/createProject.js
@@ -0,0 +1,220 @@
+window.addEventListener("DOMContentLoaded", (event) => {
+  const projectsData = [
+    {
+      title: "Trofare",
+      image: "./assets/imageicon/Trofare.jpeg",
+      type: "Mobile",
+      description: "Trofare aims at digitizing public transportation in developing countries ( Africa ). \n At Trofare, we are building a smart public transport system that improve commuters’ productivity, reduce road congestion, reduce wait time, improve road safety and reduce pollution ( CO2 and CO emissions through ride sharing ).",
+      appLink: {
+        name: "Trofare",
+        url: "https://play.google.com/store/apps/details?id=com.aimscommunity.trofare",
+      },
+      tools: ["Flutter", "Nodejs", "MongoDb", "Firebase","Reactjs"],
+      otherLink: [
+        {
+          name: "Trofare",
+          url: "https://play.google.com/store/apps/details?id=com.aimscommunity.trofare",
+        },
+        {
+          name: "Trofare Admin",
+          url: "https://play.google.com/store/apps/details?id=com.aimscommunity.trofareAdmin",
+        },
+        {
+          name: "Trofare Driver",
+          url: "https://play.google.com/store/apps/details?id=com.aimscommunity.trofare",
+        },
+        {
+          name: "Trofare Intercity",
+          url: "https://play.google.com/store/apps/details?id=com.aimscommunity.trofare",
+        },
+      ],
+    },
+    {
+      title: "SalvageMe",
+      image: "./assets/imageicon/SalvageMeLanding.JPG",
+      type: "Web",
+      description: "SalvageMe is a dedicated NGO focused on promoting literacy, education, and environmental conservation through its innovative approach.\n By collecting and redistributing new and gently used educational materials, including physical books and e-books, we aim to reduce the need for new book production, thus helping to conserve trees and mitigate carbon emissions associated with book production and disposal.",
+      appLink: { name: "SalvageMe", url: "https://salvage-me.vercel.app/" },
+      tools: ["React", "Nodejs"],
+      otherLink: [
+        { name: "Ayoba", url: "www.google.com" },
+        { name: "Admin", url: "https://salvage-me-admin.vercel.app/" },
+      ],
+    },
+    {
+      title: "C-Auth",
+      image: "./assets/imageicon/cAuth.JPG",
+      type: "Web",
+      description: "A Smart Contract for awarding government projects and payment.",
+      appLink: { name: "C-Auth", url: "https://c-auth.vercel.app/" },
+      tools: ["React", "Nodejs", "Solidity"],
+      otherLink: [
+        {
+          name: "C-Auth",
+          url: "https://c-auth.vercel.app/",
+        },
+      ],
+    },
+  ];
+
+
+  let main = document.querySelector(".profileMain");
+
+  let projectsDataMapRes = projectsData.map((project) => CreateHelper(project));
+
+  projectsDataMapRes.map((projectElement) => {
+    main.appendChild(projectElement);
+  });
+
+  function CreateHelper(project) {
+    let projectImage = document.createElement("div");
+    projectImage.className = "projectImage";
+    projectImage.classList.add(`${project.type == "Mobile" ?"imageMobile":"imageWeb"}`)
+    projectImage.style.backgroundSize = "cover";
+    projectImage.style.backgroundRepeat = "no-repeat";
+    projectImage.style.backgroundImage = `url(${project.image})`;
+
+    let imageContainer = document.createElement("div");
+    imageContainer.className = "imageContainer";
+    imageContainer.appendChild(projectImage);
+
+    let projectTitle = document.createElement("p");
+    projectTitle.className = "projectTitle";
+    projectTitle.innerText = project.title;
+
+    let projectType = document.createElement("div");
+    projectType.className = "projectType";
+    projectType.innerHTML = project.type;
+
+    let _contentHeader1 = document.createElement("p");
+    _contentHeader1.className = "contentHeader";
+    _contentHeader1.innerText = "Application Description";
+
+    let projectDescriptionContent = document.createElement("p");
+    projectDescriptionContent.className = "projectDescriptionContent";
+    projectDescriptionContent.innerText = project.description;
+
+    let _projectInnerContainer1 = document.createElement("div");
+    _projectInnerContainer1.className = "projectInnerContainer";
+
+    _projectInnerContainer1.appendChild(_contentHeader1);
+    _projectInnerContainer1.appendChild(projectDescriptionContent);
+
+    let _contentHeader2 = document.createElement("p");
+    _contentHeader2.className = "contentHeader";
+    _contentHeader2.innerText = "Application Link";
+
+    let projectLink = document.createElement("a");
+    projectLink.className = "projectLink";
+    projectLink.href = project.appLink.url;
+    projectLink.innerHTML = project.appLink.name;
+
+    let _projectInnerContainer2 = document.createElement("div");
+    _projectInnerContainer2.className = "projectInnerContainer";
+
+    _projectInnerContainer2.appendChild(_contentHeader2);
+    _projectInnerContainer2.appendChild(projectLink);
+
+    let _contentHeader3 = document.createElement("p");
+    _contentHeader3.className = "contentHeader";
+    _contentHeader3.innerText = "Tools:";
+
+    let projectListMapRes = project.tools.map((tool) => toolsHelper(tool));
+
+    let projectList1 = document.createElement("div");
+    projectList1.className = "projectList";
+    projectListMapRes.forEach((element) => {
+      projectList1.appendChild(element);
+    });
+
+    let _projectInnerContainer4 = document.createElement("div");
+    _projectInnerContainer4.className = "projectInnerContainer";
+    _projectInnerContainer4.classList.add("hideContent");
+    _projectInnerContainer4.appendChild(_contentHeader3);
+    _projectInnerContainer4.appendChild(projectList1);
+
+    let _contentHeader4 = document.createElement("p");
+    _contentHeader4.className = "contentHeader";
+    _contentHeader4.innerText = "Other Links:";
+
+    let otherLinksMapRes = project.otherLink.map((other) =>
+      otherLinksHelper(other)
+    );
+
+    let projectList2 = document.createElement("div");
+    projectList2.className = "projectList";
+ 
+    otherLinksMapRes.forEach((element) => {
+        projectList2.appendChild(element);
+    });
+
+    let _projectInnerContainer3 = document.createElement("div");
+    _projectInnerContainer3.className = "projectInnerContainer";
+
+    _projectInnerContainer3.classList.add("hideContent");    
+    _projectInnerContainer3.appendChild(_contentHeader4);
+    _projectInnerContainer3.appendChild(projectList2);
+
+
+
+    let toggle = false;
+    const viewMore = document.createElement("p");
+    viewMore.classList.add("viewMore");
+    viewMore.innerText = "View More";
+    viewMore.addEventListener("click", (e) => {
+      e.preventDefault();
+
+      toggle = !toggle;
+
+      if (toggle) {
+        _projectInnerContainer4.classList.remove("hideContent");
+        _projectInnerContainer3.classList.remove("hideContent");
+      } else {
+        _projectInnerContainer4.classList.add("hideContent");
+        _projectInnerContainer3.classList.add("hideContent");
+      }
+    });
+
+    let _contentContainer = document.createElement("div");
+    _contentContainer.className = "contentContainer";
+    _contentContainer.appendChild(projectTitle);
+    _contentContainer.appendChild(projectType);
+    _contentContainer.appendChild(_projectInnerContainer1);
+    _contentContainer.appendChild(_projectInnerContainer2);
+    _contentContainer.appendChild(_projectInnerContainer4);
+    _contentContainer.appendChild(_projectInnerContainer3);
+    _contentContainer.appendChild(viewMore);
+
+    let projectContainer = document.createElement("div");
+    projectContainer.className = "myProject";
+
+    projectContainer.appendChild(imageContainer);
+    projectContainer.appendChild(_contentContainer);
+
+    return projectContainer;
+  }
+
+  function toolsHelper(tool) {
+    let ligther = document.createElement("div");
+    ligther.className = "ligther";
+    let p = document.createElement("p");
+    p.innerText = tool;
+    let projectToolItem = document.createElement("div");
+    projectToolItem.className = "projectToolItem";
+    projectToolItem.appendChild(ligther);
+    projectToolItem.appendChild(p);
+    return projectToolItem;
+  }
+
+  function otherLinksHelper(other) {
+    let a = document.createElement("a");
+    a.href = other.url;
+    a.innerHTML = other.name;
+
+    let projectOtherLinkItem = document.createElement("div");
+    projectOtherLinkItem.className = "projectOtherLinkItem";
+    projectOtherLinkItem.appendChild(a);
+
+    return projectOtherLinkItem;
+  }
+});
